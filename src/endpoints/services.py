d8a9c2610c06099c17ee012b9a2be1d247215b86
Shcherbatov Bogdan <shcherbatov@firecode-team.ru>
2023-10-24 17:49:46 +0300

    bugfix

--- src/endpoints/services.py
+++ src/endpoints/services.py
@@ -1,6 +1,8 @@
 from typing import List
 from fastapi import APIRouter, Depends
 
+
+from src.services.base import CrudService
 from src.schemas.services import ServicesInfo, CreateServiceScheme, UpdateServiceScheme
 from src.services.services import services_service
 from src.repository.filter.services import services_filter
@@ -12,7 +14,7 @@ services_router = APIRouter()
 async def get_services(
         service=Depends(services_service)
 ):
-    """Получение списка услуг"""
+    """Метод для получения списка услуг"""
     return await service._list(services_filter)
 
 
@@ -21,4 +23,23 @@ async def create_service(
         data: CreateServiceScheme,
         service=Depends(services_service)
 ):
+    """Метод для создания услуги"""
     return await service._create(data)
+
+
+@services_router.put('/{service_id}', response_model=UpdateServiceScheme)
+async def update_service(
+        service_id: int,
+        data: UpdateServiceScheme,
+        service=Depends(services_service)
+):
+    """Метод для обновления услуги"""
+    return await service._update(data=data, entity_id=service_id)
+
+
+@services_router.delete('/{service_id}')
+async def delete_service(
+        service_id: int,
+        service: CrudService = Depends(services_service)
+):
+    return await service._delete(entity_id=service_id)
